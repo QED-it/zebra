8eec5a5fa8538095e9b664b04c2d31abfcdaa8ec
Dmitry Demin <dmitry@qed-it.com>
2025-03-15 09:38:32 +0100

    Remove TrustedPreallocate impl and size constants from orchard_zsa::issuance module as they are not used (librutzcash serialization is used)

--- zebra-chain/src/orchard_zsa/issuance.rs
+++ zebra-chain/src/orchard_zsa/issuance.rs
@@ -2,50 +2,50 @@
 
 use std::{fmt::Debug, io};
 
-use halo2::pasta::pallas;
-
 // For pallas::Base::from_repr only
 use group::ff::PrimeField;
 
-use zcash_primitives::transaction::components::issuance::{read_v6_bundle, write_v6_bundle};
+use halo2::pasta::pallas;
 
 use orchard::{
-    issuance::{IssueAction, IssueBundle, Signed},
+    issuance::{IssueBundle, Signed},
     note::ExtractedNoteCommitment,
-    Note,
 };
 
-use crate::{
-    block::MAX_BLOCK_BYTES,
-    serialization::{SerializationError, TrustedPreallocate, ZcashDeserialize, ZcashSerialize},
-};
+use zcash_primitives::transaction::components::issuance::{read_v6_bundle, write_v6_bundle};
 
-use super::burn::ASSET_BASE_SIZE;
+use crate::serialization::{SerializationError, ZcashDeserialize, ZcashSerialize};
 
 /// Wrapper for `IssueBundle` used in the context of Transaction V6. This allows the implementation of
 /// a Serde serializer for unit tests within this crate.
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct IssueData(IssueBundle<Signed>);
 
-// Sizes of the types, in bytes
-// FIXME: import from orchard
-const ADDRESS_SIZE: u64 = 43;
-const NULLIFIER_SIZE: u64 = 32;
-const NOTE_VALUE_SIZE: u64 = 4;
-const RANDOM_SEED_SIZE: u64 = 32;
-// FIXME: is this a correct way to calculate (simple sum of sizes of components)?
-const NOTE_SIZE: u64 =
-    ADDRESS_SIZE + NOTE_VALUE_SIZE + ASSET_BASE_SIZE + NULLIFIER_SIZE + RANDOM_SEED_SIZE;
-
-impl TrustedPreallocate for Note {
-    fn max_allocation() -> u64 {
-        (MAX_BLOCK_BYTES - 1) / NOTE_SIZE
+impl IssueData {
+    /// Returns a reference to the inner `IssueBundle<Signed>`.
+    pub fn inner(&self) -> &IssueBundle<Signed> {
+        &self.0
     }
 }
 
-impl TrustedPreallocate for IssueAction {
-    fn max_allocation() -> u64 {
-        (MAX_BLOCK_BYTES - 1) / 3
+impl From<IssueBundle<Signed>> for IssueData {
+    fn from(inner: IssueBundle<Signed>) -> Self {
+        Self(inner)
+    }
+}
+
+impl IssueData {
+    pub(crate) fn note_commitments(&self) -> impl Iterator<Item = pallas::Base> + '_ {
+        self.0.actions().iter().flat_map(|action| {
+            action.notes().iter().map(|note| {
+                // FIXME: Make `ExtractedNoteCommitment::inner` public in `orchard` (this would
+                // eliminate the need for the workaround of converting `pallas::Base` from bytes
+                // here), or introduce a new public method in `orchard::issuance::IssueBundle` to
+                // retrieve note commitments directly from `orchard`.
+                pallas::Base::from_repr(ExtractedNoteCommitment::from(note.commitment()).to_bytes())
+                    .unwrap()
+            })
+        })
     }
 }
 
@@ -55,7 +55,7 @@ impl ZcashSerialize for Option<IssueData> {
     }
 }
 
-// FIXME: We can't split IssueData out of Option<IssueData> deserialization,
+// We can't split IssueData out of Option<IssueData> deserialization,
 // because the counts are read along with the arrays.
 impl ZcashDeserialize for Option<IssueData> {
     fn zcash_deserialize<R: io::Read>(reader: R) -> Result<Self, SerializationError> {
@@ -63,10 +63,10 @@ impl ZcashDeserialize for Option<IssueData> {
     }
 }
 
-#[cfg(any(test, feature = "proptest-impl"))]
+#[cfg(any(test, feature = "proptest-impl", feature = "elasticsearch"))]
 impl serde::Serialize for IssueData {
-    fn serialize<S: serde::Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+    fn serialize<S: serde::Serializer>(&self, _serializer: S) -> Result<S::Ok, S::Error> {
         // TODO: FIXME: implement Serde serialization here
-        "(IssueData)".serialize(serializer)
+        unimplemented!("Serde serialization for IssueData not implemented");
     }
 }
