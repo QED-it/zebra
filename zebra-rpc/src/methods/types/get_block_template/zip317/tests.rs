54ffabe7531cf4a40fa776be8d7921645d5bf7f6
Dmitry Demin <dmitry@qed-it.com>
2025-08-25 13:25:19 +0200

    Fix compilation error in zebra-rpc/src/methods/types/get_block_template/zip317/tests.rs (because Address::try_from_raw_transparent_p2pkh was removed in librustzcash)

--- zebra-rpc/src/methods/types/get_block_template/zip317/tests.rs
+++ zebra-rpc/src/methods/types/get_block_template/zip317/tests.rs
@@ -1,8 +1,14 @@
 //! Tests for ZIP-317 transaction selection for block template production
 
+use zcash_address::TryFromAddress;
 use zcash_keys::address::Address;
 
-use zebra_chain::{block::Height, parameters::Network, transaction, transparent::OutPoint};
+use zebra_chain::{
+    block::Height,
+    parameters::{Network, NetworkKind},
+    transaction,
+    transparent::OutPoint,
+};
 use zebra_node_services::mempool::TransactionDependencies;
 
 use super::select_mempool_transactions;
@@ -13,8 +19,10 @@ fn excludes_tx_with_unselected_dependencies() {
     let next_block_height = Height(1_000_000);
     let extra_coinbase_data = Vec::new();
     let mut mempool_tx_deps = TransactionDependencies::default();
+    // FIXME: is this a correct change (try_from_raw_transparent_p2pkh to try_from_transparent_p2pkh)?
     let miner_address =
-        Address::try_from_raw_transparent_p2pkh([0x7e; 20]).expect("should be a valid address");
+        Address::try_from_transparent_p2pkh(NetworkKind::from(network.clone()).into(), [0x7e; 20])
+            .expect("should be a valid address");
     let unmined_tx = network
         .unmined_transactions_in_blocks(..)
         .next()
@@ -44,8 +52,10 @@ fn includes_tx_with_selected_dependencies() {
     let network = Network::Mainnet;
     let next_block_height = Height(1_000_000);
     let unmined_txs: Vec<_> = network.unmined_transactions_in_blocks(..).take(3).collect();
+    // FIXME: is this a correct change (try_from_raw_transparent_p2pkh to try_from_transparent_p2pkh)?
     let miner_address =
-        Address::try_from_raw_transparent_p2pkh([0x7e; 20]).expect("should be a valid address");
+        Address::try_from_transparent_p2pkh(NetworkKind::from(network.clone()).into(), [0x7e; 20])
+            .expect("should be a valid address");
 
     let dependent_tx1 = unmined_txs.first().expect("should have 3 txns");
     let dependent_tx2 = unmined_txs.get(1).expect("should have 3 txns");
